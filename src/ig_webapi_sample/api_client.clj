869e9a0e0bbd0ac1227be7371d75b0535506ff89
Lucas Gut <lucaschueko@yahoo.es>
2015-11-08 23:00:29 +0000

    - Define stateful context var...need to make it thread-local.

--- src/ig_webapi_sample/api_client.clj
+++ src/ig_webapi_sample/api_client.clj
@@ -4,40 +4,44 @@
   (:require [clj-http.client :as client])
   (:import (ig_webapi_sample.client.request Context)))
 
+(def context (atom nil))
+(defn set-context [new-context]
+  (reset! context new-context))
+
 (defn authenticate [username password apikey environment]
-  (login/login (Context. username password apikey environment)))
+  (set-context (login/login (Context. username password apikey environment))))
 
-(defn get-positions [app]
-  (request/get-generator app "/positions" 2))
+(defn get-positions [context]
+  (request/get-generator context "/positions" 2))
 
-(defn get-working-orders [app]
-  (request/get-generator app "/workingorders" 2))
+(defn get-working-orders [context]
+  (request/get-generator context "/workingorders" 2))
 
-(defn get-open-sprints [app]
-  (request/get-generator app "/positions/sprintmarkets" 2))
+(defn get-open-sprints [context]
+  (request/get-generator context "/positions/sprintmarkets" 2))
 
-(defn get-watchlists [app]
-  (request/get-generator app "/watchlists" 1))
+(defn get-watchlists [context]
+  (request/get-generator context "/watchlists" 1))
 
-(defn get-accounts [app]
-  (request/get-generator app "/accounts" 1))
+(defn get-accounts [context]
+  (request/get-generator context "/accounts" 1))
 
-(defn get-transactions [app]
-  (request/get-generator app "/history/transactions" 2))
+(defn get-transactions [context]
+  (request/get-generator context "/history/transactions" 2))
 
-(defn get-applications [app]
-  (request/get-generator app "/operations/application" 1))
+(defn get-applications [context]
+  (request/get-generator context "/operations/contextlication" 1))
 
-(defn get-epics [app epic]
-  (request/get-generator app (str "/markets/" epic)  3))
+(defn get-epics [context epic]
+  (request/get-generator context (str "/markets/" epic)  3))
 
-(defn get-market-sentiment [app marketId]
-  (request/get-generator app (str "/clientsentiment/" marketId) 1))
+(defn get-market-sentiment [context marketId]
+  (request/get-generator context (str "/clientsentiment/" marketId) 1))
 
-(defn find-epic [app search-term]
-  (request/get-with-query-params-generator app "/markets" 1 {"searchTerm" search-term }))
+(defn find-epic [context search-term]
+  (request/get-with-query-params-generator context "/markets" 1 {"searchTerm" search-term }))
 
 (defrecord Otc-limit-order [direction epic size level expiry currencyCode forceOpen orderType])
 
-(defn create-otc-order [app body]
-  (request/post-generator app "/positions/otc" (client/json-encode body)  2))
+(defn create-otc-order [context body]
+  (request/post-generator context "/positions/otc" (client/json-encode body)  2))
--- src/ig_webapi_sample/api_sample.clj
+++ src/ig_webapi_sample/api_sample.clj
@@ -8,7 +8,9 @@
   (if (not= (count args) 4)
     (println "Syntax: <username> <password> <api key> [:test|:uat|:demo|:live]")
     (do
-      (println "Authenticating...")
-      (let [response (apiclient/authenticate (first args) (second args) (nth args 2) (nth args 3))]
-        (pprint response))
-      )))
+      (println ">>> Authenticating user" (first args))
+      (pprint (apiclient/authenticate (first args) (second args) (nth args 2) (nth args 3)))
+
+      (println ">>> Retrieving positions for accountId=" (:currentAccountId @apiclient/context))
+      (let [response (apiclient/get-positions @apiclient/context)]
+        (pprint response)))))
